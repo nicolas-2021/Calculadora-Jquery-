7db706e109a9dc000b3f3477cdafed5f0b9f38c3
nicolas-2021 <nicolasmartinamuedo1@gmail.com>
2023-01-22 20:18:22 -0300

    Commit-Jquery

--- src/index.js
+++ src/index.js
@@ -1,12 +1,4 @@
 import './index.css';
-import React from 'react';
-import ReactDOM  from 'react-dom/client';
-import {Componente} from './Componente';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
- <Componente />
-);
 
 
 
